99becf0c6570a1137061638a5bfebdc2aceb8d7a
Maximkalenkovich <maxim.peterson@mail.ru>
2023-12-13 11:28:35 +0300

    add function sum for sum a & b

--- src/01/value01.tsx
+++ src/01/value01.tsx
@@ -6,7 +6,17 @@ const sentense = ' hello my friends'
 const sayHello =  () => {
     console.log(sentense)
 }
-
 sayHello()
+
+const splitIntoWords = (sentense: string) => {
+    return ("hello,my,friends")
+}
+
+
+//function sum
+function sum(a: number,b: number)  {
+    return a+b
+}
+console.log(sum(3,7))
 
 export const value01 = '01_01'
