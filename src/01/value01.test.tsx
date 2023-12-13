0f8c04819f15e87d20b078bbc2ef5250e1a6a580
Maximkalenkovich <maxim.peterson@mail.ru>
2023-12-13 12:08:24 +0300

    add tests

--- src/01/value01.test.tsx
+++ src/01/value01.test.tsx
@@ -1,11 +1,35 @@
-import {sum} from "./value01";
+import {mult, splitIntoWords, sum} from "./value01";
 
 export {}
 
 test('sum be corret', ()=>{
     const a = 1
-    const b = 3
+    const b = 2
+    const result1 = sum(a,b)
+    expect(result1).toBe(3)
 
-    const result = sum(a,b)
-    expect(result).toBe(3)
+})
+
+test('mult be corret', ()=>{
+    const a = 1
+    const b = 2
+    const result1 = mult(a,b)
+    expect(result1).toBe(2)
+})
+
+test('spliting into words correct', ()=>{
+    const  sent1 = 'hello my friend'
+    const sent2 =  'js programing language!'
+
+    const  result1 = splitIntoWords(sent1)
+    const  result2 = splitIntoWords(sent2)
+     expect(result1.length).toBe(3);
+    expect(result1[0]).toBe('hello')
+    expect(result1[1]).toBe('my')
+    expect(result1[2]).toBe('friend')
+
+    expect(result2.length).toBe(3);
+    expect(result2[0]).toBe('js')
+    expect(result2[1]).toBe('programing')
+    expect(result2[2]).toBe('language')
 })
--- src/01/value01.tsx
+++ src/01/value01.tsx
@@ -1,22 +1,24 @@
 
 
 
-const sentense = ' hello my friends'
+export const sentense = 'hello my friend'
 
-const sayHello =  () => {
-    console.log(sentense)
-}
-sayHello()
+export const splitIntoWords = (sentense: string) => {
+    return sentense.toLowerCase().split(' ')
+        .filter(w=> w !== '')
+        .map(w => w.replace('!',''))
 
-const splitIntoWords = (sentense: string) => {
-    return ("hello,my,friends")
 }
 
 
 //function sum
 export function sum(a: number,b: number)  {
-    return a+b
+    return a + b
 }
-console.log(sum(3,7))
+
+export function mult (a: number, b: number) {
+    return a * b
+}
+
 
 export const value01 = '01_01'
